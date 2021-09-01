8eb2728319124f75d087881f8e6f9104912d6ab8
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2021-09-01 13:14:30 -0500

    update ResetScheduler.php

--- src/JonyGamesYT9/MineResetAuto/ResetScheduler.php
+++ src/JonyGamesYT9/MineResetAuto/ResetScheduler.php
@@ -1,1 +1,30 @@
+<?php
 
+namespace JonyGamesYT9\MineResetAuto;
+
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\scheduler\Task;
+use pocketmine\Player;
+use pocketmine\Server;
+use function str_replace;
+
+/**
+* Class ResetScheduler
+* @package JonyGamesYT9\MineResetAuto
+*/
+class ResetScheduler extends Task
+{
+
+  /**
+  * @param int $tick
+  */
+  public function onRun(int $tick)
+  {
+    foreach (Server::getInstance()->getOnlinePlayers() as $players) {
+      if ($players instanceof Player) {
+        $players->sendMessage(str_replace(["&"], ["§"], MineResetAuto::getInstance()->getResetMessage()));
+      }
+    }
+    Server::getInstance()->dispatchCommand(new ConsoleCommandSender(), "mine reset-all");
+  }
+}
